de00de6f3b122d5909bfaf9919a58fbb82d54dac
Trevor Jones <trevordavidjones@gmail.com>
2016-07-22 16:31:34 -0400

    added all RESTful action, routes and views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,42 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    render "index.html.erb"
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image],
+      description: params[:description]
+    )
+    @product.save
+    render "create.html.erb"
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render "show.html.erb"
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    render "edit.html.erb"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image],
+      description: params[:description]
+    )
+    render "update.html.erb"
   end
 end
